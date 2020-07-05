523781d94dd8628f64f6c0eb334eaa83a264bfa9
Steven Charles <65751942+stevenCharles1325@users.noreply.github.com>
2020-07-05 23:23:11 +0800

    Update send.php

--- src/send.php
+++ src/send.php
@@ -7,13 +7,8 @@
 		$message = $_POST['message'];
 		$to = 'stevencharles1325@gmail.com';
 
-		if(mail($to, $subject, $message, $email)){
-			echo "<script type="text/javascript"> alert('successful')</script>";
-		}
-		else
-		{
-			echo "<script type="text/javascript"> alert('Something went wrong...')</script>";
-		}
+		mail($to, $subject, $message, $email)
+			
 	}
 
 
